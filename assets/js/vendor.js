5e03d6345c8664e3cbff52af803c21836d1c8a0b
muhammamdsurya <muhammadsurya2812@gmail.com>
2023-06-12 19:02:21 +0700

    update02

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -10,44 +10,6 @@ window.addEventListener("load", () => {
   });
 });
 
-// Swiper product index
-var swiper = new Swiper(".slide-container", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  sliderPerGroup: 4,
-  loop: true,
-  centerSlide: true,
-  fade: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 3000, // Mengatur delay antara slide (dalam milidetik)
-    disableOnInteraction: false, // Membuat autoplay tetap berjalan saat pengguna melakukan interaksi dengan swiper
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1000: {
-      slidesPerView: 4,
-    },
-  },
-});
-
 /**
  * Testimonials slider
  */
@@ -82,10 +44,5 @@ new Swiper(".portfolio-details-slider", {
     clickable: true,
   },
 });
-
-//parallax
-var image = document.getElementsByClassName("img-thumbnail");
-new simpleParallax(image);
-
 // Pure counter
 new PureCounter();
